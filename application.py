0ccdb45272a3ee3819e43595fb6242931f52659a
Sindhya-git <57254190+Sindhya-git@users.noreply.github.com>
2019-12-12 16:29:40 +0530

    Update application.py

--- application.py
+++ application.py
@@ -19,8 +19,8 @@ class Storage():
       user   = os.getenv('MYSQL_USER', "xxuser"),
       passwd = os.getenv('MYSQL_PASSWORD', "welcome1")
       db     = os.getenv('MYSQL_DATABASE',"sampledb"),
-      host   = os.getenv(MYSQL_SERVICE_HOST', "custom-mysql.gamification.svc.cluster.local"),
-      port   = int(os.getenv(''MYSQL_SERVICE_PORT',"3306"))
+      host   = os.getenv('MYSQL_SERVICE_HOST', "custom-mysql.gamification.svc.cluster.local"),
+      port   = int(os.getenv('MYSQL_SERVICE_PORT',"3306"))
     )
     print(" init successfull")
 
