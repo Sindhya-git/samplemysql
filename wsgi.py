19619a767275e3055969ef42d3ea67a3e21d7c80
Sindhya-git <57254190+Sindhya-git@users.noreply.github.com>
2019-12-13 15:39:09 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -28,10 +28,10 @@ class Storage():
 
     cur = self.db.cursor()
     cur.execute("SELECT * FROM XXIBM_PRODUCT_STYLE")
-    row = cur.fetchone()
+    row = cur.fetchall()
     print("Total number of row in PRODUCT_SKU is: ", cur.rowcount)
     print("Item Number = ", row, )
-    print(row[0])
+    print(row[0],"  ", row[1])
     return row[0], row[1], row[2], row[3], row[4]
     
 if __name__ == "__main__":
