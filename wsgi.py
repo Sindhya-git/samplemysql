f7b3f3235842bde1b7eddb86efe9bad0dfa25aed
Sindhya-git <57254190+Sindhya-git@users.noreply.github.com>
2019-12-17 20:44:24 +0530

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, json, request, session, redirect
+import MySQLdb
+import os
+
+#Initialize flask
+application = Flask(__name__)
+
+@application.route("/")
+def home_page():
+  return render_template("home.html")
+
+def get_dbdata():
+  storage = Storage()
+  score = storage.score()
+  return score
+
+class Storage():
+
+  def __init__(self):
+
+    self.db = MySQLdb.connect(
+      user   = "xxuser",
+      passwd = "welcome1",
+      db     ="sampledb",
+      host   = "custom-mysql.gamification.svc.cluster.local",
+      port   = int('3306')
+    )
+    cur = self.db.cursor()
+
+  def score(self):
+
+    cur = self.db.cursor()
+    cur.execute("SELECT * FROM XXIBM_PRODUCT_SKU")
+    row = cur.fetchall()
+
+    return row[0]
+
+if __name__ == "__main__":
+    application.run()
