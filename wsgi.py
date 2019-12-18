72d0d747c665af71288e7e9918ad79b823bdae70
Sindhya-git <57254190+Sindhya-git@users.noreply.github.com>
2019-12-18 21:58:41 +0530

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,25 +1,51 @@
 from flask import Flask, render_template, json, request, session, redirect
+#from wtforms import Form, BooleanField, StringField, PasswordField, validators
 import MySQLdb
 import os
 
 #Initialize flask
-application = Flask(__name__)
+app = Flask(__name__)
+app.config['IMAGES_DEST'] = 'static/images'
 
-@application.route("/")
+# Config MySQL
+mysql = MySQL()
+app.config['MYSQL_HOST'] = "custom-mysql.gamification.svc.cluster.local"
+app.config['MYSQL_USER'] = "xxuser"
+app.config['MYSQL_PASSWORD'] = "welcome1"
+app.config['MYSQL_DB'] = "sampledb"
+app.config['MYSQL_PORT'] = int('3306')
+
+# Initialize the app for use with this MySQL class
+mysql.init_app(app)
+
+
+@app.route("/")
 def home_page():
-  return render_template("home.html")
+  #form = LoadForm(request.form)
+  print("inside home page")  
+  cur = mysql.connection.cursor()
+  values = 'Shirts'
+  cur.execute("SELECT COMMODITY_NAME FROM XXIBM_PRODUCT_CATALOGUE WHERE CLASS_NAME=%s ORDER BY RAND() LIMIT 1", (values,))
+  shirts = cur.fetchall()
+  print("shirt fetched")
+  values = 'Shoes'
+  cur.execute("SELECT COMMODITY_NAME FROM XXIBM_PRODUCT_CATALOGUE WHERE CLASS_NAME=%s ORDER BY RAND() LIMIT 1", (values,))
+  shoes = cur.fetchall()
+  values = 'Luggage'
+  cur.execute("SELECT COMMODITY_NAME FROM XXIBM_PRODUCT_CATALOGUE WHERE CLASS_NAME=%s ORDER BY RAND() LIMIT 1", (values,))
+  luggage = cur.fetchall()
+  return render_template('home.html', shirts=shirts, shoes=shoes, luggage=luggage, form=form)
 
-@application.route("/data")
+@app.route("/search",methods=['GET', 'POST'])
 def get_dbdata():
-  storage = Storage()
+  db = db_init()
   score = storage.score()
   return score
 
-class Storage():
+class dbinit():
 
   def __init__(self):
-    
-    print("initializing DB")
+
     self.db = MySQLdb.connect(
       user   = "xxuser",
       passwd = "welcome1",
@@ -34,8 +60,8 @@ class Storage():
     cur = self.db.cursor()
     cur.execute("SELECT * FROM XXIBM_PRODUCT_SKU")
     row = cur.fetchall()
-    print("row is ", row)
-    return str(row)
+
+    return row[0]
 
 if __name__ == "__main__":
     application.run()
