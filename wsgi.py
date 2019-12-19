e7271b77846268c3cb1b4fc6c96ef1a85d79e47a
Sindhya-git <57254190+Sindhya-git@users.noreply.github.com>
2019-12-19 09:58:57 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -10,27 +10,21 @@ application = Flask(__name__)
 
 @application.route("/")
 def home_page():
-  self.db = MySQLdb.connect(
-      user   = "xxuser",
-      passwd = "welcome1",
-      db     ="sampledb",
-      host   = "custom-mysql.gamification.svc.cluster.local",
-      port   = int('3306')
-    )
-  cur = self.db.cursor()
+   product = dbget()
+    
   #form = LoadForm(request.form)
   print("inside home page")  
   values = 'Shirts'
-  cur.execute("SELECT COMMODITY_NAME FROM XXIBM_PRODUCT_CATALOGUE WHERE CLASS_NAME=%s ORDER BY RAND() LIMIT 1", (values,))
-  shirts = cur.fetchall()
+  #cur.execute("SELECT COMMODITY_NAME FROM XXIBM_PRODUCT_CATALOGUE WHERE CLASS_NAME=%s ORDER BY RAND() LIMIT 1", (values,))
+  shirts = product.score()
   print("shirt fetched:", shirts)
   values = 'Shoes'
-  cur.execute("SELECT COMMODITY_NAME FROM XXIBM_PRODUCT_CATALOGUE WHERE CLASS_NAME=%s ORDER BY RAND() LIMIT 1", (values,))
-  shoes = cur.fetchall()
+  #cur.execute("SELECT COMMODITY_NAME FROM XXIBM_PRODUCT_CATALOGUE WHERE CLASS_NAME=%s ORDER BY RAND() LIMIT 1", (values,))
+  shoes = product.score()
   print("shoes fetched:", shoes)
   values = 'Luggage'
-  cur.execute("SELECT COMMODITY_NAME FROM XXIBM_PRODUCT_CATALOGUE WHERE CLASS_NAME=%s ORDER BY RAND() LIMIT 1", (values,))
-  luggage = cur.fetchall()
+  #cur.execute("SELECT COMMODITY_NAME FROM XXIBM_PRODUCT_CATALOGUE WHERE CLASS_NAME=%s ORDER BY RAND() LIMIT 1", (values,))
+  luggage = product.score()
   print("luggage  fetched:", luggage)
   return render_template('home.html', shirts=shirts, shoes=shoes, luggage=luggage)
 
@@ -51,13 +45,13 @@ class dbget():
     )
     cur = self.db.cursor()
 
- # def score(self):
+ def score(self):
 
-  #  cur = self.db.cursor()
-   # cur.execute("SELECT * FROM XXIBM_PRODUCT_SKU")
-   # row = cur.fetchall()
+  cur = self.db.cursor()
+  cur.execute("SELECT COMMODITY_NAME FROM XXIBM_PRODUCT_CATALOGUE WHERE CLASS_NAME=%s ORDER BY RAND() LIMIT 1", (values,))
+  row = cur.fetchall()
 
-   # return row[0]
+  return str(row)
 
 if __name__ == "__main__":
     application.run()
