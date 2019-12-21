61526afd39209f883e54baa14fdb7487929c7754
Sindhya-git <57254190+Sindhya-git@users.noreply.github.com>
2019-12-21 12:41:37 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -49,12 +49,9 @@ class dbget():
     cur = self.db.cursor()
     print("in score",)
     values = 'Clothing'
-    cur.execute("SELECT COMMODITY,COMMODITY_NAME FROM XXIBM_PRODUCT_CATALOGUE WHERE FAMILY_NAME=%s LIMIT 1", (values,))
+    cur.execute("SELECT string(COMMODITY),string(COMMODITY_NAME) FROM XXIBM_PRODUCT_CATALOGUE WHERE FAMILY_NAME=%s LIMIT 1", (values,))
     row = cur.fetchall()
     print("row is:", row)
-    (commodity, commodity_name) = row
-    print("commodity is:", commodity)
-    print("commodity_name is:", commodity_name)
     return str(row)
 
 if __name__ == "__main__":
