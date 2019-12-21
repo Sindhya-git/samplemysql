a2b86953a7c22fa777e8c9de8747dc8a6d4be5bd
Sindhya-git <57254190+Sindhya-git@users.noreply.github.com>
2019-12-21 14:00:29 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -32,30 +32,6 @@ def home_page():
   return render_template('home.html', shirts=shirts)
 
 
-def get_dbdata():
-  db = dbget()
-  
-class dbget():
-
-  def __init__(self):
-
-    self.db = MySQLdb.connect(
-      user   = "xxuser",
-      passwd = "welcome1",
-      db     ="sampledb",
-      host   = "custom-mysql.gamification.svc.cluster.local",
-      port   = int('3306')
-    )
-    cur = self.db.cursor()
-
-  def score(self):
-    cur = self.db.cursor()
-    print("in score",)
-    values = 'Clothing'
-    cur.execute("SELECT COMMODITY,COMMODITY_NAME INTO @commodity , @commodity_name FROM XXIBM_PRODUCT_CATALOGUE WHERE FAMILY_NAME=%s LIMIT 1", (values,))
-    row = cur.fetchall()
-    print("row is:", row )
-    return str(row)
-
+    
 if __name__ == "__main__":
     application.run()
