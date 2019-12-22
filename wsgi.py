eaf1a7595f085697b9fd3591f59fede98425abd1
Sindhya-git <57254190+Sindhya-git@users.noreply.github.com>
2019-12-22 11:17:06 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -50,7 +50,7 @@ def search():
         commosrch = cur3.fetchall()
         cur3.close()    # Close cursor3
         cur4 = mysql.connection.cursor()
-        for commo in productsrch:
+        for commo in commosrch:
           print ("commo is:", commosrch['COMMODITY'])
           cur4.execute("SELECT s.ITEM_NUMBER, s.DESCRIPTION,s.LONG_DESCRIPTION, s.SKU_ATTRIBUTE_VALUE1,s.SKU_ATTRIBUTE_VALUE2,p.LIST_PRICE,p.DISCOUNT FROM XXIBM_PRODUCT_SKU s INNER JOIN XXIBM_PRODUCT_PRICING p WHERE s.ITEM_NUMBER=p.ITEM_NUMBER and CATALOGUE CATEGORY = %s LIMIT 25", (commosrch['COMMODITY'],))
           productsrch = cur4.fetchall()
