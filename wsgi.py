e21562ff748b92562be0b66c21e7a76214ca9ecc
Sindhya-git <57254190+Sindhya-git@users.noreply.github.com>
2019-12-22 10:41:52 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -31,14 +31,35 @@ def home_page():
   else:
     print("inside home page",)  
     cur1 = mysql.connection.cursor()
-    values = 'Clothing'
     cur1.execute("SELECT s.ITEM_NUMBER, s.DESCRIPTION,s.LONG_DESCRIPTION, s.SKU_ATTRIBUTE_VALUE1,s.SKU_ATTRIBUTE_VALUE2,p.LIST_PRICE,p.DISCOUNT FROM XXIBM_PRODUCT_SKU s INNER JOIN XXIBM_PRODUCT_PRICING p WHERE s.ITEM_NUMBER=p.ITEM_NUMBER LIMIT 25")
     shirts = cur1.fetchall()
  # Close Connection
     cur1.close()
     return render_template('home.html', shirts=shirts)
 
-  
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    if 'q' in request.args:
+        q = request.args['q']
+        print ("q is :",q)
+        # Create cursor
+        cur3 = mysql.connection.cursor()
+        # Get message
+        query_string = "SELECT * FROM XXIBM_PRODUCT_CATALOGUE WHERE COMMODITY_NAME LIKE %s ORDER BY COMMODITY ASC"
+        cur3.execute(query_string, ('%' + q + '%',))
+        commosrch = cur3.fetchall()
+        cur3.close()    # Close cursor3
+        cur4 = mysql.connection.cursor()
+        for commo in productsrch:
+          cur4.execute("SELECT s.ITEM_NUMBER, s.DESCRIPTION,s.LONG_DESCRIPTION, s.SKU_ATTRIBUTE_VALUE1,s.SKU_ATTRIBUTE_VALUE2,p.LIST_PRICE,p.DISCOUNT FROM XXIBM_PRODUCT_SKU s INNER JOIN XXIBM_PRODUCT_PRICING p WHERE s.ITEM_NUMBER=p.ITEM_NUMBER and CATALOGUE CATEGORY=%s" LIMIT 25", (commo['COMMODITY'],)))
+          productsrch = cur4.fetchall()
+          cur4.close()
+        #end-for  
+        flash('Showing result for: ' + q, 'success')
+        return render_template('search.html', product_srch=productsrch)
+    else:
+        flash('Search again', 'danger')
+        return render_template('search.html')  
 
     
 if __name__ == "__main__":
